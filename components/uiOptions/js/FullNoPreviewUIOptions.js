0d5ec226a16ac3a43e24f5de5915bb2800d61575
Ramil Gonzaga <ramil.gonzaga@gmail.com>
2013-09-19 10:23:25 -0700

    first commit

---
+++ components/uiOptions/js/FullNoPreviewUIOptions.js
@@ -0,0 +1,1 @@
+var fluid_1_5=fluid_1_5||{};(function($,fluid){fluid.defaults("fluid.uiOptions.fullNoPreview",{gradeNames:["fluid.uiOptions.inline","autoInit"],container:"{fullNoPreview}.container",templateLoader:{options:{templates:{uiOptions:"%prefix/FullNoPreviewUIOptions.html"}}},uiOptions:{options:{listeners:{onReset:function(uiOptions){uiOptions.save()}}}}})})(jQuery,fluid_1_5);
